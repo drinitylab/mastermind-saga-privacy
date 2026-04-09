b20d88ccbf9eb25076e8165a48ec1dcdeb3c090c
drinitylab <drinitylab@gmail.com>
2026-04-09 22:11:09 +0200

    Add version.js with app version and changelog

---
+++ version.js
@@ -0,0 +1,7 @@
+{
+  "version": "2.1.1",
+  "url": "https://play.google.com/store/apps/details?id=com.drinitylab.mastermindsaga",
+  "changelog": [
+    "Added full translations for Spanish, French, and German to match the languages ​​supported by the auto-detection feature."
+  ]
+}
